c7dfefb3395833acd205dd29bdc87d0b339ecfb9
ephayeabe <ephremyeabe@gmail.com>
2021-10-04 14:44:24 +0300

    0x09. C - Static libraries

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -22,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif 
+#endif /* _HOLBERTON_H_ */
