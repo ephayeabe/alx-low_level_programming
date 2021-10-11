45d71cf6dc59c476d297127d5a1a73159a4ffd00
ephayeabe <ephremyeabe@gmail.com>
2021-10-11 10:53:58 +0300

    modify 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,31 +1,47 @@
-#include"main.h"
-int is_pal_rec(char *str, int s, int e)
-{
-     
-    if (s == e){
-		return 1;
-	}
- 
-    if (str[s] != str[e]){
-		return 	0;
-	}
+#include "main.h"
 
-    if (s < e + 1){
-		return is_pal_rec(str, s + 1, e - 1);
-	}
- 
-    return 1;
+/**
+ * _strlen - return length of string
+ * @str: string to check
+ *
+ * Return: length of str
+ */
+int _strlen(char *str)
+{
+	if (*str == '\0')
+		return (0);
+	else
+		return (1 + _strlen(str + 1));
 }
- 
+
+/**
+ * check_palindrome - checks to see if a string is a palindrome
+ * @l: left hand index
+ * @r: right hand index
+ * @p: possible palindrome
+ *
+ * Return: 1 if palindrome 0 if not
+ */
+int check_palindrome(int l, int r, char *p)
+{
+	if (l >= r)
+		return (1);
+	else if (p[l] != p[r])
+		return (0);
+	else
+		return (check_palindrome(l + 1, r - 1, p));
+}
+
+/**
+ * is_palindrome - states if a string is a palindrome
+ * @s: string to check
+ *
+ * Return: 1 if palindrome, 0 if not
+ */
 int is_palindrome(char *s)
 {
-    int n;
-	n = strlen(s);
-    
-    if (n == 0){
-        return 1;
-	}
-     
-    return is_pal_rec(s, 0, n - 1);
+	int i;
+
+	i = _strlen(s) - 1;
+	return (check_palindrome(0, i, s));
 }
- 
