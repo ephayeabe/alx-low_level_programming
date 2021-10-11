85d5404b1928e20778cee25ecc1d2b841d4a7de0
ephayeabe <ephremyeabe@gmail.com>
2021-10-11 10:51:31 +0300

    modify 6-is_print_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,17 +1,31 @@
 #include "main.h"
 
-int current_diviser = 2;
-
-int is_prime_number(int n) {
-    if (n <= 2) {
-        return (n == 2) ? 1 : 0;
-    }
-    if (n % current_diviser == 0) {
-        return 0;
-    }
-    if (current_diviser * current_diviser > n)
-        return 1;
-    current_diviser++;
-    return is_prime_number(n);
+/**
+ * prime_check - checks to see if number is prime
+ * @f: factor check
+ * @p: possible prime number
+ *
+ * Return: 1 if prime, 0 if not
+ */
+int prime_check(int f, int p)
+{
+	if (p < 2 || p % f == 0)
+		return (0);
+	else if (f > p / 2)
+		return (1);
+	else
+		return (prime_check(f + 1, p));
 }
 
+/**
+ * is_prime_number - states if number is prime
+ * @n: number to check
+ *
+ * Return: 1 if prime, 0 if not
+ */
+int is_prime_number(int n)
+{
+	if (n == 2)
+		return (1);
+	return (prime_check(2, n));
+}
