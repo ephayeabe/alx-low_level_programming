1aae0bda2f3af4f36ef7c29ceefcadf341fbbe7d
ephayeabe <ephremyeabe@gmail.com>
2021-10-13 10:18:23 +0300

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,29 +2,26 @@
 
 /**
  * wildcmp - compares two strings
- * @s1: first string
- * @s2: second string, may contain wildcard *
- *
- * Return: 1 if match, 0 if not
- */
+ * @s1: 1st string to be compared
+ * @s2: second string containing '*' or not
+ * Return: 1 if the strings can be considered as identical, and 0
+ **/
 int wildcmp(char *s1, char *s2)
 {
-	char *p;
-
-	if (*s2 == '*' && *(s2 + 1) == '\0')
-		return (1);
-	else if (*s2 == '*' && *(s2 + 1) == '*')
-		return (wildcmp(s1, s2 + 1));
-	else if (*s2 == '*')
-	{
-		p = wildcard(s1, s2 + 1);
-		if (p == 0)
-			return (0);
-		return (wildcmp(p + 1, s2 + 2));
-	}
-	else if (*s1 != *s2)
-		return (0);
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	return (wildcmp(s1 + 1, s2 + 1));
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '\0')
+		return (0);
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	if (*s1 == '\0')
+	{
+		if (*s2 != '*')
+			return (0);
+		else
+			return (wildcmp(s1, s2 + 1));
+	}
+	return (0);
 }
