6d179dce35866346800f14ebb5c73378d5f3e003
ephayeabe <ephremyeabe@gmail.com>
2021-10-13 09:15:28 +0300

    modify 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,34 +1,25 @@
-int wildcmp(char *s1, char *s2){
-	
-}
-
-int main(void)
+/**
+ * wildcmp - compares two strings
+ * @s1: 1st string to be compared
+ * @s2: second string containing '*' or not
+ * Return: 1 if the strings can be considered as identical, and 0
+ **/
+int wildcmp(char *s1, char *s2)
 {
-    int r;
-
-    r = wildcmp("main.c", "*.c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m*a*i*n*.*c*");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "main.c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m*c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "ma********************************c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "*");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "***");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m.*c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "**.*c");
-    printf("%d\n", r);
-    r = wildcmp("main-main.c", "ma*in.c");
-    printf("%d\n", r);
-    r = wildcmp("main", "main*d");
-    printf("%d\n", r);
-    r = wildcmp("abc", "*b");
-    printf("%d\n", r);
-    return (0);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '\0')
+		return (0);
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	if (*s1 == '\0')
+	{
+		if (*s2 != '*')
+			return (0);
+		else
+			return (wildcmp(s1, s2 + 1));
+	}
+	return (0);
 }
